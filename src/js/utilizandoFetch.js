acdc6147c830c15b5ae4d2be98acd0933d9fbdf0
HugoVinicius <hugo.desenvolvimento@outlook.com>
2019-06-29 17:39:09 -0300

    Utilizando Fetch API laboratório 3

---
+++ src/js/utilizandoFetch.js
@@ -0,0 +1,36 @@
+const autores = [{ nome: 'André', git: 'lordonebr' }, { nome: 'Hugo Vinicius', git: 'hugovinicius' }];
+
+var rodapeDocumento = document.querySelector('.rodape ul');
+
+$(document).ready(function () {
+    for (autor of autores) {
+        BuscarDadosGit(autor);
+    }
+});
+
+function BuscarDadosGit(autor) {
+    url = `https://api.github.com/users/${autor.git}/repos`,
+        fetchData = {
+            method: 'GET',
+            headers: new Headers()
+        };
+
+    fetch(url, fetchData)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length == 0) {
+                alert(`Repositorio vazio: ${data}`);
+                console.log(data);
+            } else {
+                CarregarDadosGit(autor, data[0]);
+            }
+        });
+}
+
+function CarregarDadosGit(autor, repositorio) {
+   let elementLI = document.createElement('li'),
+       htmlAutor = `<a href="https://github.com/${autor.git}" target="_blank"><img alt="" width="260" height="260" class="" src=" ${repositorio.owner.avatar_url}"><br>${autor.nome}</a>`;
+
+   elementLI.innerHTML = htmlAutor;
+   rodapeDocumento.appendChild(elementLI);
+}
